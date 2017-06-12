2ab6f7145a4d85155c21672a3c3a8d51b19f5f34
Rogerio J. Gentil <rogeriogentil@ufscar.br>
2017-06-12 11:02:39 -0300

    Alteração para remover instanciação do código compilado
    - Adição de dependência do Log4j;
    - Adição de arquivo de propriedades para indicar classes a serem instanciadas; e
    - Alteração para que a instanciação da classe não seja fixa no código compilado.

--- src/main/java/rogeriogentil/cditest/service/CalculadoraDeSalariosFactory.java
+++ src/main/java/rogeriogentil/cditest/service/CalculadoraDeSalariosFactory.java
@@ -1,5 +1,8 @@
 package rogeriogentil.cditest.service;
 
+import java.util.ResourceBundle;
+import org.apache.log4j.Logger;
+
 /**
  * Classe para auxiliar a implementação do padrão <em>Strategy</em> no
  * cálculo de salários.
@@ -8,12 +11,18 @@ package rogeriogentil.cditest.service;
  */
 public class CalculadoraDeSalariosFactory {
     
+    private static Logger logger;
+    private static ResourceBundle bundle = ResourceBundle.getBundle("dependencias");
+    
     public static CalculadoraDeSalarios criarCalculadora() {
-        TabelaDeReferenciaSalarial pisosSalariais = new TabelaDeReferenciaSalarialPadrao();
+        String nomeDaClasse = bundle.getString("calculadora_salarios");
         
-        CalculadoraDeSalarios calculadora = new CalculadoraDeSalarios2017();        
-        calculadora.setTabelaDeReferenciaSalarial(pisosSalariais);
-        
-        return calculadora;
+        try {
+            Class<?> clazz = Class.forName(nomeDaClasse);
+            return clazz.asSubclass(CalculadoraDeSalarios.class).newInstance();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            logger.error(e);
+            return null;
+        }
     }
 }
